b5f0e26feb13b409b51ae978a85eac67c595fd61
Ajinkya.Patil <Ajinkya.Patil@APATIL10>
2023-04-28 15:01:43 +0530

    Assignment4 Programs

---
+++ C#LearningGit/Assignment 3/Shape_2D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_C_.Exercise_Day2
+{
+    public abstract class Shape_2D
+    {
+        double length, width;
+        public Shape_2D(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public void Area(double length)
+        {
+            double area = length * length;
+            Console.WriteLine("Area of the given square with length as " + length + " is " + area);
+        }
+
+        public void Perimeter(double side1, double side2, double baselength)
+        {
+            double perimeter = side1 + side2 + baselength;
+            Console.WriteLine("Perimeter of a Triangle is " + perimeter);
+        }
+    }
+}
---
+++ C#LearningGit/Assignment 3/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Training_C_.Exercise_Day2;
+
+namespace Training_C_.Exercise_Day3
+{
+    public class Square : Shape_2D
+    {
+        public Square(double side ,double length, double width) : base(length, width)
+        {
+        }
+
+        public void Area(double length)
+        {
+            double area = length * length;
+            Console.WriteLine("Area of the given square with length as " + length + " is " + area);
+        }
+
+        public void Perimeter(double side1, double side2, double baselength)
+        {
+            double perimeter = side1 + side2 + baselength;
+            Console.WriteLine("Perimeter of a Triangle is " + perimeter);
+        }
+    }
+}
