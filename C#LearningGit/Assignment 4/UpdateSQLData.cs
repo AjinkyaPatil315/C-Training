92c489c6540f4ae15cd787758b6594ffe246b8f2
Ajinkya.Patil <Ajinkya.Patil@APATIL10>
2023-05-03 12:31:31 +0530

    SqlReadWritePrograms

--- C#LearningGit/Assignment 4/UpdateSQLData.cs
+++ C#LearningGit/Assignment 4/UpdateSQLData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,25 @@ namespace C_LearningGit.Assignment_4
 {
     internal class UpdateSQLData
     {
+        public void UpdateSQLData1() 
+        {
+            var salary = 105;
+            var empId = 1;
+            var read = new ReadSQlData();
+            var oldvalue = read.ReadSQlDataAssign("Salary", "SELECT TOP 1 * FROM Employee Where EmpId = " + empId);
+            string source = "Data Source=localhost\\SQLEXPRESS02;Database=master;" + "Integrated Security=True;" + "TrustServerCertificate=True;";
+            string select = "UPDATE Employee SET Salary = " + salary + " Where EmpId = " + empId;
+            using (SqlConnection conn = new SqlConnection(source))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(select, conn);
+                int rowsCount = cmd.ExecuteNonQuery();
+                Console.WriteLine("the no of rows updated is {0}", rowsCount);
+                conn.Close();
+            }
+            var newvalue = read.ReadSQlDataAssign("Salary", "SELECT TOP 1 * FROM Employee Where EmpId = " + empId);
+            Console.WriteLine($"Old Value {oldvalue}, New Value {newvalue}");
+        }
     }
 }
--- C#LearningGit/RunMainClass.cs
+++ C#LearningGit/RunMainClass.cs
@@ -16,95 +16,96 @@ namespace Training_C_
     {
         static void Main()
         {
-            //Assignment 1 programs 
-            Consonants consonants = new Consonants();
-            Console.WriteLine("Assignment-1 : First Program \n ");
-            consonants.findConsonants("Rrrrrajhhanssa".ToLower());//Input value should be from Console And Handle special character
-            Fibonacci fibonacci = new Fibonacci();
-            Console.WriteLine("Assignment-1 : Second Program \n ");
-            fibonacci.findFibonacciSeries(3, 40);
-            Pyramid pyramid = new Pyramid();
-            Console.WriteLine("Assignment-1 : Third Program \n ");
-            pyramid.PrintPyramid(28);
-            ReverseInteger reverse = new ReverseInteger();
-            Console.WriteLine("Assignment-1 : Fourth Program \n ");
-            reverse.reverseInteger(123456);
-            SquareRoot root = new SquareRoot();
-            Console.WriteLine("Assignment-1 : Fifth Program \n ");
-            root.findSquareRoot(7);
-            root.findSquareRoot(50);
-            root.findSquareRoot(21);
+            ////Assignment 1 programs 
+            //Consonants consonants = new Consonants();
+            //Console.WriteLine("Assignment-1 : First Program \n ");
+            //consonants.findConsonants("Rrrrrajhhanssa".ToLower());//Input value should be from Console And Handle special character
+            //Fibonacci fibonacci = new Fibonacci();
+            //Console.WriteLine("Assignment-1 : Second Program \n ");
+            //fibonacci.findFibonacciSeries(3, 40);
+            //Pyramid pyramid = new Pyramid();
+            //Console.WriteLine("Assignment-1 : Third Program \n ");
+            //pyramid.PrintPyramid(28);
+            //ReverseInteger reverse = new ReverseInteger();
+            //Console.WriteLine("Assignment-1 : Fourth Program \n ");
+            //reverse.reverseInteger(123456);
+            //SquareRoot root = new SquareRoot();
+            //Console.WriteLine("Assignment-1 : Fifth Program \n ");
+            //root.findSquareRoot(7);
+            //root.findSquareRoot(50);
+            //root.findSquareRoot(21);
 
 
-            //Assignment 2 programs
-            AreaPerimeter formula = new AreaPerimeter();
-            Console.WriteLine("Assignment-2 : First Program \n ");
-            formula.AreaOfTriangle(11, 24);
-            formula.AreaOfSquare(10);
-            formula.AreaOfRectangle(28, 12);
+            ////Assignment 2 programs
+            //AreaPerimeter formula = new AreaPerimeter();
+            //Console.WriteLine("Assignment-2 : First Program \n ");
+            //formula.AreaOfTriangle(11, 24);
+            //formula.AreaOfSquare(10);
+            //formula.AreaOfRectangle(28, 12);
 
-            formula.PerimeterOftriangle(23, 21, 39);
-            formula.CircumferenceOfCircle(21);
-            formula.PerimeterOfRectangle(49, 69);
+            //formula.PerimeterOftriangle(23, 21, 39);
+            //formula.CircumferenceOfCircle(21);
+            //formula.PerimeterOfRectangle(49, 69);
 
-            StudentExamMarks stu = new StudentExamMarks(75, 56, 39, 80);
+            //StudentExamMarks stu = new StudentExamMarks(75, 56, 39, 80);
+            //Console.WriteLine("\n");
+            //Console.WriteLine("Assignment-2 : Second Program \n ");
+            //stu.Name = "Ajinkya Patil";
+            //stu.Address = "Pune";
+            //stu.GetTotalMarks();
+            //stu.GetAverageMarks();
+            //stu.GetPercentage();
+
+            ////Assignment 3 program
+            //Console.WriteLine("\n");
+            //Console.WriteLine("Assignment-3 : First Program \n ");
+            //Square square = new Square(15, 15, 15);
+            //square.Area(16);
+            //square.Perimeter(10, 10, 10, 10);
+            //Rectangle rect = new Rectangle(30, 40);
+            //rect.Area();
+            //rect.Perimeter();
+            //Cuboid cuboid = new Cuboid(7, 7, 10);
+            //cuboid.Area();
+            //Cube cube = new Cube(3.5, 3.5, 1);
+            //cube.Area();
+
+
+
+            //Console.WriteLine("\n");
+            //Console.WriteLine("Assignment-3 : Second Program \n ");
+            //Maths math = new Maths();
+            //math.Sum((long)18, (long)17);
+            //math.Sum(250, 201, 159);
+            ////double output = maths.SumProduct(20, 35);
+            ////Console.WriteLine("Sum Product returns sum of the values = " + output);
+            //math.Difference(105, 1515);
+            //math.Difference(20.25, 30.35, 25);
+            //math.Division(1000, 25);
+            //math.Division(1000, 0);
+            //math.Division(10, 10, 2);
+            //math.Division(10, 10, 0);
+            //math.Product(25, 25);
+            //math.Product(10, 10, 5);
+            //math.Modulus(1258, 100);
+            //math.Modulus(12588, 1256, 100);
+
+
+            ////Assignment 4 program
+            //Console.WriteLine("\n");
+            //Console.WriteLine("Assignment-4 : First Program \n ");
+            //new ReadExcel().ReadExcelData();
+
+            //Console.WriteLine("\n");
+            //Console.WriteLine("Assignment-4 : Second Program \n ");
+            //new UpdateExcelData().UpdateExcelDataAssign();
+
+
             Console.WriteLine("\n");
-            Console.WriteLine("Assignment-2 : Second Program \n ");
-            stu.Name = "Ajinkya Patil";
-            stu.Address = "Pune";
-            stu.GetTotalMarks();
-            stu.GetAverageMarks();
-            stu.GetPercentage();
+            Console.WriteLine("Assignment-4 : Fourth Program \n ");
+            new UpdateSQLData().UpdateSQLData1();
 
-            //Assignment 3 program
-            Console.WriteLine("\n");
-            Console.WriteLine("Assignment-3 : First Program \n ");
-            Square square = new Square(15, 15, 15);
-            square.Area(16);
-            square.Perimeter(10, 10, 10, 10);
-            Rectangle rect = new Rectangle(30, 40);
-            rect.Area();
-            rect.Perimeter();
-            Cuboid cuboid = new Cuboid(7, 7, 10);
-            cuboid.Area();
-            Cube cube = new Cube(3.5, 3.5, 1);
-            cube.Area();
-
-
-
-            Console.WriteLine("\n");
-            Console.WriteLine("Assignment-3 : Second Program \n ");
-            Maths math = new Maths();
-            math.Sum((long)18, (long)17);
-            math.Sum(250, 201, 159);
-            //double output = maths.SumProduct(20, 35);
-            //Console.WriteLine("Sum Product returns sum of the values = " + output);
-            math.Difference(105, 1515);
-            math.Difference(20.25, 30.35, 25);
-            math.Division(1000, 25);
-            math.Division(1000, 0);
-            math.Division(10, 10, 2);
-            math.Division(10, 10, 0);
-            math.Product(25, 25);
-            math.Product(10, 10, 5);
-            math.Modulus(1258, 100);
-            math.Modulus(12588, 1256, 100);
-
-
-            //Assignment 4 program
-            Console.WriteLine("\n");
-            Console.WriteLine("Assignment-4 : First Program \n ");
-            new ReadExcel().ReadExcelData();
-
-            Console.WriteLine("\n");
-            Console.WriteLine("Assignment-4 : Second Program \n ");
-            new UpdateExcelData().UpdateExcelDataAssign();
-
-            Console.WriteLine("\n");
-            Console.WriteLine("Assignment-4 : Third Program \n ");
-            new ReadSQlData().ReadSQlDataAssign();
-
-            //Assignment 5 (i) program
+            //Assignment 5 program
         }
     }
 }
